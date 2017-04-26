fa644f733affa30c8fb68f7f8668d94ccdc29d35
Kyle Sharp <kyle.sharp@metova.com>
2017-04-26 17:29:12 -0500

    Added in a recycler view for the movie list, pulled in Butterknife, and used Butterknife to bind it

--- app/src/main/java/com/kpsharp/omdbsearch/MainActivity.java
+++ app/src/main/java/com/kpsharp/omdbsearch/MainActivity.java
@@ -1,14 +1,33 @@
 package com.kpsharp.omdbsearch;
 
+import com.kpsharp.omdbsearch.movielist.MovieListAdapter;
+
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.RecyclerView;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
 
 public class MainActivity extends AppCompatActivity {
+
+    @BindView(R.id.movie_recycler_view)
+    RecyclerView mMovieRecyclerView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
 
         super.onCreate(savedInstanceState);
+
         setContentView(R.layout.activity_main);
+
+        loadViews();
+    }
+
+    private void loadViews() {
+
+        ButterKnife.bind(this);
+
+        mMovieRecyclerView.setAdapter(new MovieListAdapter());
     }
 }
---
+++ app/src/main/java/com/kpsharp/omdbsearch/movielist/MovieListAdapter.java
@@ -0,0 +1,26 @@
+package com.kpsharp.omdbsearch.movielist;
+
+import android.support.v7.widget.RecyclerView;
+import android.view.ViewGroup;
+
+public class MovieListAdapter extends RecyclerView.Adapter {
+
+    @Override
+    public RecyclerView.ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+
+        return null;
+    }
+
+    @Override
+    public void onBindViewHolder(RecyclerView.ViewHolder holder, int position) {
+
+    }
+
+    @Override
+    public int getItemCount() {
+
+        return 0;
+    }
+
+
+}
