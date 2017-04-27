0c525ce0119a3f0e6dc1524bcaea67d1167f1951
Kyle Sharp <kyle.sharp@metova.com>
2017-04-26 19:19:28 -0500

    Added a SearchView for searching

--- app/src/main/java/com/kpsharp/omdbsearch/ui/MainActivity.java
+++ app/src/main/java/com/kpsharp/omdbsearch/ui/MainActivity.java
@@ -6,19 +6,23 @@ import com.kpsharp.omdbsearch.ui.movielist.MovieListAdapter;
 import com.kpsharp.omdbsearch.ui.movielist.MovieListMvp;
 import com.kpsharp.omdbsearch.ui.movielist.MovieListPresenter;
 
+import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
 import android.support.v7.widget.RecyclerView;
+import android.support.v7.widget.SearchView;
 
 import java.util.List;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
 
-public class MainActivity extends AppCompatActivity implements MovieListMvp.View {
+public class MainActivity extends AppCompatActivity implements MovieListMvp.View, SearchView.OnQueryTextListener {
 
     // region Variables
+
+    @BindView(R.id.movie_search_view)
+    SearchView mMovieTitleSearchView;
 
     @BindView(R.id.movie_recycler_view)
     RecyclerView mMovieRecyclerView;
@@ -66,12 +70,36 @@ public class MainActivity extends AppCompatActivity implements MovieListMvp.View
 
         ButterKnife.bind(this);
 
+        mMovieTitleSearchView.setQueryHint(getResources().getString(R.string.movie_list_search_view_hint));
+        mMovieTitleSearchView.setOnQueryTextListener(this);
+
         mMovieListAdapter = new MovieListAdapter(this);
 
         mMovieRecyclerView.setAdapter(mMovieListAdapter);
     }
 
     // endregion
+    // endregion
+
+    // region OnClickListener
+
+    @Override
+    public boolean onQueryTextChange(String newText) {
+
+        // no-op
+        return false;
+    }
+
+    @Override
+    public boolean onQueryTextSubmit(String query) {
+
+        mMovieTitleSearchView.clearFocus();
+
+        mMovieListPresenter.userSearchedForMovie(query);
+
+        return true;
+    }
+
     // endregion
 
     // region MovieListMvp.View
